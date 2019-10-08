4ebe51ff7de618190dd7d756c1099711f09e67c3
siddacious <nospam187+github@gmail.com>
2019-10-08 14:38:06 -0700

    extracted the lsm303DLH accelerometer code

---
+++ Adafruit_LSM303_Accel.cpp
@@ -0,0 +1,215 @@
+/***************************************************************************
+  This is a library for the LSM303 Accelerometer and magnentometer/compass
+
+  Designed specifically to work with the Adafruit LSM303DLHC Breakout
+
+  These displays use I2C to communicate, 2 pins are required to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Bryan Siepert for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
+#include <limits.h>
+
+#include "Adafruit_LSM303_Accel.h"
+
+/* enabling this #define will enable the debug print blocks
+#define LSM303_DEBUG
+*/
+
+static float _lsm303Accel_MG_LSB     = 0.001F;   // 1, 2, 4 or 12 mg per lsb
+
+/***************************************************************************
+ ACCELEROMETER
+ ***************************************************************************/
+/***************************************************************************
+ PRIVATE FUNCTIONS
+ ***************************************************************************/
+
+/**************************************************************************/
+/*!
+    @brief  Abstract away platform differences in Arduino wire library
+*/
+/**************************************************************************/
+void Adafruit_LSM303_Accel_Unified::write8(byte address, byte reg, byte value)
+{
+  Wire.beginTransmission(address);
+  #if ARDUINO >= 100
+    Wire.write((uint8_t)reg);
+    Wire.write((uint8_t)value);
+  #else
+    Wire.send(reg);
+    Wire.send(value);
+  #endif
+  Wire.endTransmission();
+}
+
+/**************************************************************************/
+/*!
+    @brief  Abstract away platform differences in Arduino wire library
+*/
+/**************************************************************************/
+byte Adafruit_LSM303_Accel_Unified::read8(byte address, byte reg)
+{
+  byte value;
+
+  Wire.beginTransmission(address);
+  #if ARDUINO >= 100
+    Wire.write((uint8_t)reg);
+  #else
+    Wire.send(reg);
+  #endif
+  Wire.endTransmission();
+  Wire.requestFrom(address, (byte)1);
+  #if ARDUINO >= 100
+    value = Wire.read();
+  #else
+    value = Wire.receive();
+  #endif
+  Wire.endTransmission();
+
+  return value;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Reads the raw data from the sensor
+*/
+/**************************************************************************/
+void Adafruit_LSM303_Accel_Unified::read()
+{
+  // Read the accelerometer
+  Wire.beginTransmission((byte)LSM303_ADDRESS_ACCEL);
+  #if ARDUINO >= 100
+    Wire.write(LSM303_REGISTER_ACCEL_OUT_X_L_A | 0x80);
+  #else
+    Wire.send(LSM303_REGISTER_ACCEL_OUT_X_L_A | 0x80);
+  #endif
+  Wire.endTransmission();
+  Wire.requestFrom((byte)LSM303_ADDRESS_ACCEL, (byte)6);
+
+  // Wait around until enough data is available
+  while (Wire.available() < 6);
+
+  #if ARDUINO >= 100
+    uint8_t xlo = Wire.read();
+    uint8_t xhi = Wire.read();
+    uint8_t ylo = Wire.read();
+    uint8_t yhi = Wire.read();
+    uint8_t zlo = Wire.read();
+    uint8_t zhi = Wire.read();
+  #else
+    uint8_t xlo = Wire.receive();
+    uint8_t xhi = Wire.receive();
+    uint8_t ylo = Wire.receive();
+    uint8_t yhi = Wire.receive();
+    uint8_t zlo = Wire.receive();
+    uint8_t zhi = Wire.receive();
+  #endif
+
+  // Shift values to create properly formed integer (low byte first)
+  raw.x = (int16_t)(xlo | (xhi << 8)) >> 4;
+  raw.y = (int16_t)(ylo | (yhi << 8)) >> 4;
+  raw.z = (int16_t)(zlo | (zhi << 8)) >> 4;
+}
+
+/***************************************************************************
+ CONSTRUCTOR
+ ***************************************************************************/
+
+/**************************************************************************/
+/*!
+    @brief  Instantiates a new Adafruit_LSM303 class
+*/
+/**************************************************************************/
+Adafruit_LSM303_Accel_Unified::Adafruit_LSM303_Accel_Unified(int32_t sensorID) {
+  _sensorID = sensorID;
+
+  // Clear the raw accel data
+  raw.x = 0;
+  raw.y = 0;
+  raw.z = 0;
+}
+
+/***************************************************************************
+ PUBLIC FUNCTIONS
+ ***************************************************************************/
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW
+*/
+/**************************************************************************/
+bool Adafruit_LSM303_Accel_Unified::begin()
+{
+  // Enable I2C
+  Wire.begin();
+
+  // Enable the accelerometer (100Hz)
+  write8(LSM303_ADDRESS_ACCEL, LSM303_REGISTER_ACCEL_CTRL_REG1_A, 0x57);
+
+  // LSM303DLHC has no WHOAMI register so read CTRL_REG1_A back to check
+  // if we are connected or not
+  uint8_t reg1_a = read8(LSM303_ADDRESS_ACCEL, LSM303_REGISTER_ACCEL_CTRL_REG1_A);
+  if (reg1_a != 0x57)
+  {
+    return false;
+  }
+
+  return true;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Gets the most recent sensor event
+*/
+/**************************************************************************/
+bool Adafruit_LSM303_Accel_Unified::getEvent(sensors_event_t *event) {
+  /* Clear the event */
+  memset(event, 0, sizeof(sensors_event_t));
+
+  /* Read new data */
+  read();
+
+  event->version   = sizeof(sensors_event_t);
+  event->sensor_id = _sensorID;
+  event->type      = SENSOR_TYPE_ACCELEROMETER;
+  event->timestamp = millis();
+  event->acceleration.x = (float)raw.x * _lsm303Accel_MG_LSB * SENSORS_GRAVITY_STANDARD;
+  event->acceleration.y = (float)raw.y * _lsm303Accel_MG_LSB * SENSORS_GRAVITY_STANDARD;
+  event->acceleration.z = (float)raw.z * _lsm303Accel_MG_LSB * SENSORS_GRAVITY_STANDARD;
+
+  return true;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Gets the sensor_t data
+*/
+/**************************************************************************/
+void Adafruit_LSM303_Accel_Unified::getSensor(sensor_t *sensor) {
+  /* Clear the sensor_t object */
+  memset(sensor, 0, sizeof(sensor_t));
+
+  /* Insert the sensor name in the fixed length char array */
+  strncpy (sensor->name, "LSM303", sizeof(sensor->name) - 1);
+  sensor->name[sizeof(sensor->name)- 1] = 0;
+  sensor->version     = 1;
+  sensor->sensor_id   = _sensorID;
+  sensor->type        = SENSOR_TYPE_ACCELEROMETER;
+  sensor->min_delay   = 0;
+  sensor->max_value   = 0.0F; // TBD
+  sensor->min_value   = 0.0F; // TBD
+  sensor->resolution  = 0.0F; // TBD
+}
---
+++ Adafruit_LSM303_Accel.h
@@ -0,0 +1,111 @@
+/***************************************************************************
+  This is a library for the LSM303 Accelerometer and magnentometer/compass
+
+  Designed specifically to work with the Adafruit LSM303DLHC Breakout
+
+  These displays use I2C to communicate, 2 pins are required to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Bryan Siepert for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+#ifndef __LSM303_ACCEL_H__
+#define __LSM303_ACCEL_H__
+
+#if (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+
+/*=========================================================================
+    I2C ADDRESS/BITS
+    -----------------------------------------------------------------------*/
+    #define LSM303_ADDRESS_ACCEL          (0x32 >> 1)         // 0011001x
+    #define LSM303_ADDRESS_MAG            (0x3C >> 1)         // 0011110x
+/*=========================================================================*/
+
+/*=========================================================================
+    REGISTERS
+    -----------------------------------------------------------------------*/
+    typedef enum
+    {                                                     // DEFAULT    TYPE
+      LSM303_REGISTER_ACCEL_CTRL_REG1_A         = 0x20,   // 00000111   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG2_A         = 0x21,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG3_A         = 0x22,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG4_A         = 0x23,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG5_A         = 0x24,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_CTRL_REG6_A         = 0x25,   // 00000000   rw
+      LSM303_REGISTER_ACCEL_REFERENCE_A         = 0x26,   // 00000000   r
+      LSM303_REGISTER_ACCEL_STATUS_REG_A        = 0x27,   // 00000000   r
+      LSM303_REGISTER_ACCEL_OUT_X_L_A           = 0x28,
+      LSM303_REGISTER_ACCEL_OUT_X_H_A           = 0x29,
+      LSM303_REGISTER_ACCEL_OUT_Y_L_A           = 0x2A,
+      LSM303_REGISTER_ACCEL_OUT_Y_H_A           = 0x2B,
+      LSM303_REGISTER_ACCEL_OUT_Z_L_A           = 0x2C,
+      LSM303_REGISTER_ACCEL_OUT_Z_H_A           = 0x2D,
+      LSM303_REGISTER_ACCEL_FIFO_CTRL_REG_A     = 0x2E,
+      LSM303_REGISTER_ACCEL_FIFO_SRC_REG_A      = 0x2F,
+      LSM303_REGISTER_ACCEL_INT1_CFG_A          = 0x30,
+      LSM303_REGISTER_ACCEL_INT1_SOURCE_A       = 0x31,
+      LSM303_REGISTER_ACCEL_INT1_THS_A          = 0x32,
+      LSM303_REGISTER_ACCEL_INT1_DURATION_A     = 0x33,
+      LSM303_REGISTER_ACCEL_INT2_CFG_A          = 0x34,
+      LSM303_REGISTER_ACCEL_INT2_SOURCE_A       = 0x35,
+      LSM303_REGISTER_ACCEL_INT2_THS_A          = 0x36,
+      LSM303_REGISTER_ACCEL_INT2_DURATION_A     = 0x37,
+      LSM303_REGISTER_ACCEL_CLICK_CFG_A         = 0x38,
+      LSM303_REGISTER_ACCEL_CLICK_SRC_A         = 0x39,
+      LSM303_REGISTER_ACCEL_CLICK_THS_A         = 0x3A,
+      LSM303_REGISTER_ACCEL_TIME_LIMIT_A        = 0x3B,
+      LSM303_REGISTER_ACCEL_TIME_LATENCY_A      = 0x3C,
+      LSM303_REGISTER_ACCEL_TIME_WINDOW_A       = 0x3D
+    } lsm303AccelRegisters_t;
+
+/*=========================================================================*/
+
+/*=========================================================================
+    INTERNAL ACCELERATION DATA TYPE
+    -----------------------------------------------------------------------*/
+    typedef struct lsm303AccelData_s
+    {
+      int16_t x;
+      int16_t y;
+      int16_t z;
+    } lsm303AccelData;
+/*=========================================================================*/
+
+/*=========================================================================
+    CHIP ID
+    -----------------------------------------------------------------------*/
+    #define LSM303_ID                     (0b11010100)
+/*=========================================================================*/
+
+/* Unified sensor driver for the accelerometer */
+class Adafruit_LSM303_Accel_Unified : public Adafruit_Sensor
+{
+  public:
+    Adafruit_LSM303_Accel_Unified(int32_t sensorID = -1);
+
+    bool begin(void);
+    bool getEvent(sensors_event_t*);
+    void getSensor(sensor_t*);
+
+    lsm303AccelData raw;   // Last read accelerometer data will be available here
+
+  private:
+    int32_t         _sensorID;
+
+    void write8(byte address, byte reg, byte value);
+    byte read8(byte address, byte reg);
+    void read(void);
+};
+
+#endif
